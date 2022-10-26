e0755ee4780535b3e4cbc8e706e0f8fb81647354
YamagishiAsuka <k020g1381@g.neec.ac.jp>
2022-10-26 16:17:30 +0900

    Vector3.h改造途中

--- Code/Math/Vector3.h
+++ Code/Math/Vector3.h
@@ -1,16 +1,23 @@
 #pragma once
 #include <DirectXMath.h>
+#include "Mathf.h"
 
 using namespace DirectX;
 
-struct Vector3 : public XMFLOAT3 {
-	Vector3() = default;
+struct Vector3 : public XMFLOAT3
+{
+#pragma region コンストラクタ
+	Vector3(){
+		x = 0.0f;
+		y = 0.0f;
+		z = 0.0f;
+	}
 	Vector3(float x, float y, float z) {
 		this->x = x;
 		this->y = y;
 		this->z = z;
 	}
-	Vector3(XMFLOAT3 num) {
+	Vector3(const XMFLOAT3& num) {
 		this->x = num.x;
 		this->y = num.y;
 		this->z = num.z;
@@ -19,14 +26,32 @@ struct Vector3 : public XMFLOAT3 {
 		XMVECTOR temp = other;
 		XMStoreFloat3(this, temp);
 	}
+#pragma endregion
 
-	//演算
-	inline bool operator == (const Vector3& r) const { return x == r.x && y == r.y && z == r.z; }
-	inline bool operator != (const Vector3& r) const { return x != r.x || y != r.y || z != r.z; }
-	inline XMVECTOR operator * (const float r) const { return Vector3(x * r, y * r, z * r); }
-	inline XMVECTOR operator / (const float r) const { return Vector3(x / r, y / r, z / r); }
+#pragma region 演算子
+	//単項演算子
+	Vector3 operator +() const { return *this; }
+	Vector3 operator -() const
+	{
+		Vector3 temp = {
+			-this->x,
+			-this->y,
+			-this->z
+		};
+		return temp;
+	}
 
-	//二項演算
+	//二項演算子
+	Vector3 operator +(const Vector3& vec) const
+	{
+		Vector3 temp ={
+			this->x + vec.x,
+			this->y + vec.y,
+			this->z + vec.z
+		};
+		return temp;
+	}
+
 	Vector3 operator += (const Vector3& r) {
 		*this = *this + r;
 		return *this;
@@ -36,6 +61,14 @@ struct Vector3 : public XMFLOAT3 {
 		return *this;
 	}
 
+
+	//演算
+	inline bool operator == (const Vector3& r) const { return x == r.x && y == r.y && z == r.z; }
+	inline bool operator != (const Vector3& r) const { return x != r.x || y != r.y || z != r.z; }
+	inline XMVECTOR operator * (const float r) const { return Vector3(x * r, y * r, z * r); }
+	inline XMVECTOR operator / (const float r) const { return Vector3(x / r, y / r, z / r); }
+
+	
 	// ベクトルの内積
 	float VDot(Vector3 In) { return x * In.x + y * In.y + z * In.z; }
 	// ベクトルの外積
